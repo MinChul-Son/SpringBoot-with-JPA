cbca80e8d0d0fe82a5a212a641d57edfe6a86988
MinChul-Son <smc5236@naver.com>
2021-05-20 21:47:11 +0900

    SpringBoot & JPA : 주문, 주문상품 엔티티 개발
    주문과 주문상품 엔티티의 핵심 비즈니스 로직들을 개발했다.

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -28,7 +28,7 @@ public class Order {
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
-    private LocalDateTime orderData; // 자바8 부터는 @Temporal을 사용하지 않고 LocalDateTime을 쓰면 hibernate가 자동으로 치환
+    private LocalDateTime orderDate; // 자바8 부터는 @Temporal을 사용하지 않고 LocalDateTime을 쓰면 hibernate가 자동으로 치환
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // Enum 타입, 주문상태 [ORDER, CANCEL]
@@ -48,5 +48,49 @@ public class Order {
         this.delivery = delivery;
         delivery.setOrder(this);
     }
+
+
+    //==생성 메서드==//
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for (OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    //==비즈니스 로직==//
+
+    /**
+     * 주문 취소
+     */
+    public void cancel() {
+        if (delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for (OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    //==조회 로직==//
+
+    /**
+     * 전체 주문 가격 조회
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for (OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
+
 
 }
