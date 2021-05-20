98380b58bc53de4dc3c1ca7325bdb56ee5f62780
MinChul-Son <smc5236@naver.com>
2021-05-20 17:53:02 +0900

    SpringBoot & JPA : 상품 저장소 개발
    상품 저장, 상품 조회를 할 수 있는 상품 저장소 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) { // Item 객체만 생성된 상태에서는 id 존재 x, persist를 해주면서 id 생성
+            em.persist(item);
+        } else {
+            em.merge(item); // id 가 있으면 이미 db에 등록된 것임. ==> update
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+}
