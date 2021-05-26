2acc8336efaa9ef99e36b5d62cb5497705bd7560
MinChul-Son <smc5236@naver.com>
2021-05-26 18:39:52 +0900

    SpringBoot & JPA 2 : API 개발 기본 > 회원 수정
    커맨드와 쿼리를 분리하자!!!! 역시 update DTO를 만들어 구현하였다.
    전체 업데이트를 할때는 PUT, 부분 업데이트는 PATCH 나 POST를 사용하는
    것이 REST 스타일

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -2,11 +2,10 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -30,6 +29,28 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PostMapping("/api/v2/members/{id}") // 전체 업데이트를 할때는 PUT, 부분 업데이트는 PATCH or POST
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id); // 커맨드와 쿼리를 분리해라!!
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+
+    }
+
+    @Data
+    @AllArgsConstructor // 엔티티에는 getter만 쓰는 것이 좋고 DTO에는 setter등등 여러가지를 사용해도 괜찮다.(실용적인 면으로)
+    static class UpdateMemberResponse{
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest{
+        private String name;
     }
 
     @Data
