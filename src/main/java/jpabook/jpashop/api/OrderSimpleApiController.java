d36089b7441e9ca45035a3b1fea86d4d1562379b
MinChul-Son <smc5236@naver.com>
2021-05-28 22:04:31 +0900

    SpringBoot & JPA 2 : 간단한 주문 조회 > 엔티티를 DTO로 변환
    지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우에는 쿼리를
    생략하지만 그렇지 않은 경우에는 N번 만큼 계속 쿼리가 실행되므로
    N + 1 문제가 발생하는 성능 이슈가 발생할 수 있다.

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToone(ManyToOne, OneToOne) 관계
@@ -22,9 +27,49 @@ public class OrderSimpleApiController {
 
     private final OrderRepository orderRepository;
 
-    @GetMapping("/api/v1/sample-orders")
+    @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //Lazy 강제 초기화
+            order.getDelivery().getAddress(); //Lazy 강제 초기화
+        }
         return all;
     }
+
+    /** v2
+     * 쿼리가 총 1 + N + N번 실행된다. (v1과 쿼리수 결과는 같다.)
+     * order 조회 1번(order 조회 결과 수가(row) N이 된다.)
+     * order -> member 지연 로딩 조회 N 번
+     * order -> delivery 지연 로딩 조회 N 번
+     * 예) order의 결과가 4개면 최악의 경우 1 + 4 + 4번 실행된다.(최악의 경우)
+     * 지연로딩은 영속성 컨텍스트에서 조회하므로, 이미 조회된 경우 쿼리를 생략한다.
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화 ==> 영속성 컨텍스트에서 찾는데 없기 떄문에 db로 sql날려서 가져옴
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
+    }
+
 }
