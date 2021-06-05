a1564c87926b725c500e4e7f9e3b9997c41821fb
MinChul-Son <smc5236@naver.com>
2021-06-05 19:18:33 +0900

    JPA에서 DTO 직접 조회 : 컬렉션 조회 최적화
    JPA에서 DTO 직접 조회할 때 컬렉션에서 발생한 N+1문제를 해결해보았다.
    ToOne관계들은 먼저 조회하고, 이를 통해 얻은 식별자 id값으로 ToMany
    관계를 IN문을 사용하여 한꺼먼에 조회하였다.
    이를 MAP을 사용하여 값을 세팅하여 반환!

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 @RequiredArgsConstructor
@@ -42,4 +44,25 @@ public class OrderQueryRepository {
     }
 
 
+    public List<OrderQueryDto> findAllByDto_optimization() {
+        List<OrderQueryDto> result = findOrders();
+
+        List<Long> orderIds = result.stream() // orderId를 다 뽑아서 리스트에 담음
+                .map(o -> o.getOrderId())
+                .collect(Collectors.toList());
+
+        List<OrderItemQueryDto> orderItems = em.createQuery(
+                "select new jpabook.jpashop.repository.order.query.OrderItemQueryDto(oi.order.id, i.name, oi.orderPrice, oi.count)" +
+                        "from OrderItem oi " +
+                        " join oi.item i " +
+                        " where oi.order.id in :orderIds", OrderItemQueryDto.class) //이를 in을 통해 한번에 조회
+                .setParameter("orderIds", orderIds)
+                .getResultList();
+
+        Map<Long, List<OrderItemQueryDto>> orderItemMap = orderItems.stream()
+                .collect(Collectors.groupingBy(orderItemQueryDto -> orderItemQueryDto.getOrderId()));
+        result.forEach(o -> o.setOrderItems(orderItemMap.get(o.getOrderId())));
+        return result; // v4는 루프를 돌때마다 쿼리를 날림, v5는 쿼리를 한번 날려 모두 가져온 뒤에 메모리에서 매칭을 하여 값을 세팅
+
+    }
 }
