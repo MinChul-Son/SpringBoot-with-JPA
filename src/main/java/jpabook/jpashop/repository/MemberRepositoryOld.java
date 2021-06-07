7858943be5b27c91e70083ece56c57b91af648f0
MinChul-Son <smc5236@naver.com>
2021-06-07 22:18:55 +0900

    Spring Data JPA : 맛보기!
    인터페이스만 만들면 스프링 데이터 jpa가 알아서 다 해줌;;;; 대박

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepositoryOld.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor // 스프링 데이터 jpa에서 엔티티 메니저를 생성자 주입으로 넣을 수 있게 지원해줌.
+public class MemberRepositoryOld {
+
+//    @PersistenceContext // 엔티티 매니저 주입
+    private final EntityManager em;
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id); // find 메서드는 단건 조회, (타입, PK)
+    }
+
+    public List<Member> findAll() { // createQuery ==> (JPQL, 반환타입) , JPQL은 엔티티 객체를 대상으로
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) { // 이름으로 조회
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
